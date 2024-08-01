81744fffa2baf12bf53ac678957e624d8c40cfca
Yu Jae Hun <slalfkf0812@naver.com>
2024-08-01 14:35:24 +0900

    Var & Data Type 2

--- First/First.c
+++ First/First.c
@@ -8,6 +8,7 @@ void main(){
 }
 */
 
+/*
 void main(){
 	int a;
 	float b;
@@ -21,4 +22,23 @@ void main(){
 	printf("%d", sizeof(int));
 	printf("%d", sizeof(float));
 	printf("%d", sizeof(double));
+}
+*/
+
+void main()
+{
+	int a, b, c, d; //변수 선언
+	a = 100 + 100;
+	b = a + 100;
+	c = a + b - 100;
+	d = a + b + c;
+
+	printf("a, b , c , d 의 값 ==> %d , %d , %d , %d \n", a, b, c, d);
+	
+	a = b = c = d = 100; //a, b, c, d에 모두 100 입력
+	printf("a, b , c , d 의 값 ==> %d , %d , %d , %d \n", a, b, c, d);
+	
+	a = 100;
+	a = a + 200; //a에 a 자신과 200의 합을 대입
+	printf("a 의 값 ==> %d \n", a);
 }
