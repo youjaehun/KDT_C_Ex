0cccfdb3e6b73cd2243f1d05fe1515683a1381f6
Yu Jae Hun <slalfkf0812@naver.com>
2024-08-01 14:00:37 +0900

    Var & Data Type

--- First/First.c
+++ First/First.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+/*
 void main(){
+
 	printf("¾È³ç");
+
+}
+*/
+
+void main(){
+	int a;
+	float b;
+
+	a = 123.232;
+	b = 200;
+
+	printf("%d\n", a);
+	printf("%f\n", b);
+
+	printf("%d", sizeof(int));
+	printf("%d", sizeof(float));
+	printf("%d", sizeof(double));
 }
