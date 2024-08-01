8a0bb15c0834957e17a274453f749b89cfaf9784
Yu Jae Hun <slalfkf0812@naver.com>
2024-08-01 15:32:50 +0900

    Function

---
+++ First/ex_func.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int Sum(int value1, int value2);
+
+void main()
+{
+	int var1 = 1, var2 = -3, var3 = 5;
+	int var4 = -2, var5 = 7, var6 = -9;
+	int result1, result2, result3;
+	result1 = Sum(var1, var2);
+	result2 = Sum(var3, var4);
+	result3 = Sum(var5, var6);
+
+	printf("%d \n", result1);
+	printf("%d \n", result2);
+	printf("%d \n", result3);
+}
+
+int Sum(int value1, int value2){
+	int result = value1 + value2;
+	return result;
+}
